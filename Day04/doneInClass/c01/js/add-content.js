75b316f9366ec316a498d475cad2f76b68b16503
Trish Ladd <4132874+TrishZwei@users.noreply.github.com>
2021-11-16 12:00:44 -0800

    end of day 4

--- Day04/doneInClass/c01/js/add-content.js
+++ Day04/doneInClass/c01/js/add-content.js
@@ -1,1 +1,31 @@
 //convert to class demo method, eliminating the bad practice of document.write
+'use strict';
+
+let today = new Date();
+let hourNow = today.getHours();
+let greeting; 
+
+if(hourNow > 18){
+	greeting = 'Good Evening!';
+}else if(hourNow > 12){
+	greeting = 'Good Afternoon!';
+}else if(hourNow > 0){
+	greeting = 'Good Morning!';
+}else{
+	greeting = 'Welcome!';
+}
+
+//document.write('<h3>'+greeting+'</h3>') //from book
+
+let newEl = document.createElement('h3');
+newEl.appendChild(document.createTextNode(greeting));
+
+let ref = document.querySelector('h1');
+
+
+function insertAfter(el, referenceNode){
+	referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
+
+}
+
+insertAfter(newEl, ref)
